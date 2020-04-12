755ec64863b31574ee6d611281b39698c4fd7240
gaborvlad13 <gabor.vlad13@gmail.com>
2020-04-12 20:07:30 +0300

    Initial commit

---
+++ lab2SGBD/lab2SGBD/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace lab2SGBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewParent = new System.Windows.Forms.DataGridView();
+            this.dataGridViewChild = new System.Windows.Forms.DataGridView();
+            this.panelTextBoxes = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDisplay = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridViewChild)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+            this.dataGridViewParent.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewParent.Location = new System.Drawing.Point(12, 56);
+            this.dataGridViewParent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridViewParent.Name = "dataGridViewParent";
+            this.dataGridViewParent.RowTemplate.Height = 24;
+            this.dataGridViewParent.Size = new System.Drawing.Size(447, 342);
+            this.dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            this.dataGridViewChild.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewChild.Location = new System.Drawing.Point(487, 56);
+            this.dataGridViewChild.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridViewChild.Name = "dataGridViewChild";
+            this.dataGridViewChild.RowTemplate.Height = 24;
+            this.dataGridViewChild.Size = new System.Drawing.Size(447, 342);
+            this.dataGridViewChild.TabIndex = 1;
+            // 
+            // panelTextBoxes
+            // 
+            this.panelTextBoxes.Location = new System.Drawing.Point(963, 56);
+            this.panelTextBoxes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelTextBoxes.Name = "panelTextBoxes";
+            this.panelTextBoxes.Size = new System.Drawing.Size(294, 342);
+            this.panelTextBoxes.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 44);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Parent table";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label2.Location = new System.Drawing.Point(487, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 44);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Child table";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonConnect.Location = new System.Drawing.Point(12, 434);
+            this.buttonConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(147, 64);
+            this.buttonConnect.TabIndex = 5;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonInsert.Location = new System.Drawing.Point(346, 434);
+            this.buttonInsert.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(147, 64);
+            this.buttonInsert.TabIndex = 6;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDisplay
+            // 
+            this.buttonDisplay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonDisplay.Location = new System.Drawing.Point(180, 434);
+            this.buttonDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonDisplay.Name = "buttonDisplay";
+            this.buttonDisplay.Size = new System.Drawing.Size(147, 64);
+            this.buttonDisplay.TabIndex = 6;
+            this.buttonDisplay.Text = "Display";
+            this.buttonDisplay.UseVisualStyleBackColor = true;
+            this.buttonDisplay.Click += new System.EventHandler(this.buttonDisplay_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(508, 434);
+            this.buttonUpdate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(147, 64);
+            this.buttonUpdate.TabIndex = 7;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular,
+                System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonDelete.Location = new System.Drawing.Point(670, 434);
+            this.buttonDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(147, 64);
+            this.buttonDelete.TabIndex = 8;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 562);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDisplay);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelTextBoxes);
+            this.Controls.Add(this.dataGridViewChild);
+            this.Controls.Add(this.dataGridViewParent);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "SGBD Lab 2";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridViewChild)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Panel panelTextBoxes;
+        private System.Windows.Forms.DataGridView dataGridViewChild;
+        private System.Windows.Forms.DataGridView dataGridViewParent;
+        private System.Windows.Forms.Button buttonDisplay;
+    }
+}
---
+++ lab2SGBD/lab2SGBD/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace lab2SGBD
+{
+    public partial class Form1 : Form
+    {
+        static string con = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        SqlConnection cs = new SqlConnection(con);
+        SqlDataAdapter da = new SqlDataAdapter();
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            string parentTableName = ConfigurationManager.AppSettings["ParentTableName"];
+            da.SelectCommand = new SqlCommand("Select * from " + parentTableName, cs);
+            ds.Clear();
+            da.Fill(ds);
+            dataGridViewParent.DataSource = ds.Tables[0];
+        }
+        private void GenerateTextBoxes()
+        {
+            string childColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+            List<string> childColumnNamesList = new List<string>(childColumnNames.Split(','));
+            int l = 1;
+            foreach (var column in childColumnNamesList)
+            {
+                TextBox textBox = new TextBox();
+                Label label = new Label();
+                label.Text = column;
+                label.Top = l * 40;
+                textBox.Top = l * 40;
+                textBox.Left = 100;
+                textBox.Name = "textBox" + column;
+                panelTextBoxes.Controls.Add(label);
+                panelTextBoxes.Controls.Add(textBox);
+                l++;
+            }
+        }
+        private void buttonDisplay_Click(object sender, EventArgs e)
+        {
+            string childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+            string parentId = ConfigurationManager.AppSettings["ParentId"];
+            da.SelectCommand = new SqlCommand("Select * from " + childTableName +" where " + parentId + "=@Id", cs);
+            da.SelectCommand.Parameters.Add("@id", SqlDbType.Int).Value =
+                Convert.ToInt32(dataGridViewParent.CurrentCell.Value);
+            ds2.Clear();
+            da.Fill(ds2);
+            dataGridViewChild.DataSource = ds2.Tables[0];
+            GenerateTextBoxes();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+            string childColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+            string columnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+            SqlCommand cmd =
+                new SqlCommand("Insert into " + childTableName + " (" + childColumnNames + ") " + " values (" + columnNamesInsertParameters + ")", cs);
+            List<string> childColumnNamesList = new List<string>(childColumnNames.Split(','));
+            foreach (var column in childColumnNamesList)
+            {
+                TextBox textBox = (TextBox) panelTextBoxes.Controls["textBox" + column];
+                cmd.Parameters.AddWithValue("@" + column, textBox.Text);
+            }
+            cs.Open();
+            cmd.ExecuteNonQuery();
+            ds2.Clear();
+            da.Fill(ds2);
+            cs.Close();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+            string childColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+            string columnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+            List<string> childColumnNamesList = new List<string>(childColumnNames.Split(','));
+            List<string> columnNamesInsertParametersList = new List<string>(columnNamesInsertParameters.Split(','));
+            string toSet = "";
+            for (int i = 0; i < childColumnNamesList.Count - 1; i++)
+                toSet += childColumnNamesList[i] + "=" + columnNamesInsertParametersList[i]+", ";
+            toSet += childColumnNamesList[childColumnNamesList.Count - 1] + "=" +
+                     columnNamesInsertParametersList[columnNamesInsertParametersList.Count - 1];
+            SqlCommand cmd = new SqlCommand("Update " + childTableName + " set " + toSet + " where " + childColumnNamesList[0]+"="+columnNamesInsertParametersList[0], cs);
+            foreach (var column in childColumnNamesList)
+            {
+                TextBox textBox = (TextBox) panelTextBoxes.Controls["textBox" + column];
+                cmd.Parameters.AddWithValue("@" + column, textBox.Text);
+            }
+            cs.Open();
+            cmd.ExecuteNonQuery();
+            ds2.Clear();
+            da.Fill(ds2);
+            cs.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+            string childColumnNames = ConfigurationManager.AppSettings["ChildColumnNames"];
+            string columnNamesInsertParameters = ConfigurationManager.AppSettings["ColumnNamesInsertParameters"];
+            List<string> columnNamesInsertParametersList = new List<string>(columnNamesInsertParameters.Split(','));
+            List<string> childColumnNamesList = new List<string>(childColumnNames.Split(','));
+            SqlCommand cmd = new SqlCommand("Delete from " + childTableName + " where " + childColumnNamesList[0] + "="+ columnNamesInsertParametersList[0] , cs);
+            TextBox textBox = (TextBox) panelTextBoxes.Controls["textBox" + childColumnNamesList[0]];
+            cmd.Parameters.AddWithValue(columnNamesInsertParametersList[0], textBox.Text);
+            cs.Open();
+            cmd.ExecuteNonQuery();
+            ds2.Clear();
+            da.Fill(ds2);
+            cs.Close();
+        }
+    }
+}
